52e793b3a9e732d06e3656c043fffb3235723440
Diego Duarte <projectdcd@gmail.com>
2021-04-23 12:18:00 -0500

    CRUD completed

--- coachadmin/coachadmin/settings.py
+++ coachadmin/coachadmin/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^-73(+5n+(@s-wazp%-w57l*dclw7-$*zkev)z(c5t(9((#@t7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
